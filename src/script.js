984f20dfce7953d146a053ae0960ab3b049e6f10
Frederico Lemos <65643399+golemos@users.noreply.github.com>
2023-05-31 22:00:51 -0300

    Criada função para só exibir o campo "Tema", no fluxo "Criar Atividade", quando necessário.

--- src/script.js
+++ src/script.js
@@ -7,8 +7,8 @@ function generateJSON() {
 	let disciplina = document.querySelector("#disciplina").textContent;
 	let atividade = document.querySelector("#atividade").textContent;
 	let prazo = document.querySelector("#prazo").textContent;
-	let integrantes_min = document.querySelector("#integrantes_min").textContent;
-	let integrantes_max = document.querySelector("#integrantes_max").textContent;
+	let integrantesMin = document.querySelector("#integrantesMin").textContent;
+	let integrantesMax = document.querySelector("#integrantesMax").textContent;
 	let tema = document.querySelector("#tema").value;
 	let hora = document.querySelector("#hora").value;
 	let comentario = document.querySelector("#comentario").value;
@@ -25,8 +25,8 @@ function generateJSON() {
 		disciplina,
 		atividade,
 		prazo,
-		integrantes_min,
-		integrantes_max,
+		integrantesMin,
+		integrantesMax,
 		tema,
 		hora,
 		comentario,
@@ -73,90 +73,90 @@ function criaAtividade() {
 	}
 }
 
-    //Metodo para abrir modal
+//Oculta, em Criar Atividade, o campo "Tema", até que o usuário clique em "Sim", no campo "Tema Fixo?"
+function mostraEscondeTema() {
+	if (document.getElementById("temaFixoSim").checked) {
+		document.getElementById("temaLabel").style.display = "inline-block";
+		document.getElementById("tema").style.display = "inline-block";
+	} else {
+		document.getElementById("temaLabel").style.display = "none";
+		document.getElementById("tema").style.display = "none";
+	}
+}
+
+//Metodo para abrir modal
 function novaTurmaModal(modalName) {
-
-
-    let modal = document.getElementById(modalName);
-    if (typeof modal == 'undefined' || modal === null)
-        return;
-    modal.style.display = 'Block';
+	let modal = document.getElementById(modalName);
+	if (typeof modal == "undefined" || modal === null) return;
+	modal.style.display = "Block";
 }
-    // Metodo para fechar modal
+// Metodo para fechar modal
 function cancelarNovaTurma(modalName) {
-
-
-    let modal = document.getElementById(modalName);
-    if (typeof modal == 'undefined' || modal === null)
-        return;
-    modal.style.display = 'None';
+	let modal = document.getElementById(modalName);
+	if (typeof modal == "undefined" || modal === null) return;
+	modal.style.display = "None";
 }
 
-    //Metodo para adicionar linhas em uma tabala através da inserção de valores em um formulario. 
-function adicionarTurma ( eixo,turma,disciplina){
+//Metodo para adicionar linhas em uma tabala através da inserção de valores em um formulario.
+function adicionarTurma(eixo, turma, disciplina) {
+	//Metodo para adicionar linhas em uma tabala através da inserção de valores em um formulario.
 
-    //Metodo para adicionar linhas em uma tabala através da inserção de valores em um formulario. 
+	var tableTurma = document.getElementById("tabela-turmas");
 
-    var tableTurma = document.getElementById("tabela-turmas")
+	var rowCount = tableTurma.rows.length;
+	var row = tableTurma.insertRow(rowCount);
 
-    var rowCount = tableTurma.rows.length;
-    var row = tableTurma.insertRow(rowCount);
+	var cell1 = row.insertCell(0);
+	cell1.innerHTML = eixo;
 
-    var cell1 = row.insertCell(0);
-    cell1.innerHTML = eixo;
+	var cell2 = row.insertCell(1);
+	cell2.innerHTML = disciplina;
 
-    var cell2 = row.insertCell(1);
-    cell2.innerHTML = disciplina;
+	var cell3 = row.insertCell(2);
 
-    var cell3 = row.insertCell(2);
+	cell3.innerHTML = turma;
 
-    cell3.innerHTML = turma;
+	var cell4 = row.insertCell(3);
+	var qtdAlunos = document.createElement("a");
+	var textQtdAluno = document.createTextNode("0");
+	qtdAlunos.appendChild(textQtdAluno);
+	qtdAlunos.href = "#";
+	cell4.appendChild(qtdAlunos);
 
+	var cell5 = row.insertCell(4);
+	var iconeEditar = document.createElement("input");
+	iconeEditar.type = "image";
+	iconeEditar.src = "icones/editar.png";
+	iconeEditar.id = "icones-coluna";
 
-    var cell4 = row.insertCell(3);
-    var qtdAlunos = document.createElement('a');
-    var textQtdAluno = document.createTextNode('0');
-    qtdAlunos.appendChild(textQtdAluno);
-    qtdAlunos.href = '#';
-    cell4.appendChild(qtdAlunos);
+	var iconeExcluir = document.createElement("input");
+	iconeExcluir.type = "image";
+	iconeExcluir.src = "icones/excluir.png";
+	iconeExcluir.id = "icones-coluna";
+	iconeExcluir.onclick = function () {
+		deletarTurma(this);
+	};
+	var iconeAdcALuno = document.createElement("input");
+	iconeAdcALuno.type = "image";
+	iconeAdcALuno.src = "icones/adicionar-aluno.png";
+	iconeAdcALuno.id = "icones-coluna";
 
-    var cell5 = row.insertCell(4);
-    var iconeEditar = document.createElement('input');
-    iconeEditar.type = 'image';
-    iconeEditar.src = 'icones/editar.png';
-    iconeEditar.id = 'icones-coluna';
+	cell5.appendChild(iconeEditar);
+	cell5.appendChild(iconeExcluir);
+	cell5.appendChild(iconeAdcALuno);
 
-    var iconeExcluir = document.createElement('input');
-    iconeExcluir.type = 'image';
-    iconeExcluir.src = 'icones/excluir.png';
-    iconeExcluir.id = 'icones-coluna';
-    iconeExcluir.onclick = function(){
-       deletarTurma(this); 
-    }
-    var iconeAdcALuno = document.createElement('input');
-    iconeAdcALuno.type = 'image';
-    iconeAdcALuno.src = 'icones/adicionar-aluno.png';
-    iconeAdcALuno.id = 'icones-coluna';
-
-    cell5.appendChild(iconeEditar);
-    cell5.appendChild(iconeExcluir);
-    cell5.appendChild(iconeAdcALuno);
-   
-    alert('Disciplina ' + disciplina + ' da turma ' + turma + ' do eixo ' + eixo + ' foi criado com sucesso!');
-    cancelarNovaTurma('modal-nova-turma');
-    
-        
+	alert("Disciplina " + disciplina + " da turma " + turma + " do eixo " + eixo + " foi criado com sucesso!");
+	cancelarNovaTurma("modal-nova-turma");
 }
 
-    //Metodo para deletar linhas de uma tabela. 
-function deletarTurma ( input ){
-    var confirmacao = confirm('Deseja Realmente Excluir este Grupo?')
+//Metodo para deletar linhas de uma tabela.
+function deletarTurma(input) {
+	var confirmacao = confirm("Deseja Realmente Excluir este Grupo?");
 
-    if (confirmacao){
-    var linha = input.parentNode.parentNode;
-    var tabela = document.getElementById('tabela-turmas');
-    var numLinha = linha.rowIndex;
-    tabela.deleteRow(numLinha); 
-    }
-
+	if (confirmacao) {
+		var linha = input.parentNode.parentNode;
+		var tabela = document.getElementById("tabela-turmas");
+		var numLinha = linha.rowIndex;
+		tabela.deleteRow(numLinha);
+	}
 }
